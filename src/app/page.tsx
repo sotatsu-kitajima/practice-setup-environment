9dcdb162308e4966478217999fbbc4e6223d3f6c
sotatsu kitajima <halosakakitajima@gmail.com>
2023-02-19 16:09:00 +0900

    lintstagetesuto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <p>df</p>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
